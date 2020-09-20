47dc5e6e03ff8e4e913aa2e3e7b7babaf07d4d2d
Maksym Tarasenko <max.tarasenko.slip@gmail.com>
2020-09-21 02:40:01 +0300

    mongoose package included

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const contactsRoutes = require('./routes/contacts');
+const mongoose = require('mongoose');
+
+// Set up default mongoose connection
+mongoose.connect(
+  'mongodb+srv://dzhoi:4VZQ4hxa2uTHfLZ7@cluster0.7wmkk.mongodb.net/db-contacts?retryWrites=true&w=majority',
+  { useNewUrlParser: true },
+  err => {
+    if (err) {
+      console.log('Database connection error');
+      process.exit(1);
+    }
+    console.log('Database connection successful');
+  },
+);
+
+// Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
 const port = process.env.PORT || 3000;
